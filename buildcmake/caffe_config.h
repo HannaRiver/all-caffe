eae31715d903c1e3ef7035702d66b23d9cdf45c3
[helium] <[hena@em-data.com.cn]>
2022-02-19 21:11:07 +0800

    [add]caffe-ocr

---
+++ buildcmake/caffe_config.h
@@ -0,0 +1,12 @@
+/* Sources directory */
+#define SOURCE_FOLDER "/home/hena/caffe-ocr"
+
+/* Binaries directory */
+#define BINARY_FOLDER "/home/hena/caffe-ocr/buildcmake"
+
+/* This is an absolute path so that we can run test from any build
+ * directory */
+#define ABS_TEST_DATA_DIR "/home/hena/caffe-ocr/src/caffe/test/test_data/"
+
+/* Test device */
+#define CUDA_TEST_DEVICE -1
---
+++ buildcmake/tools/CMakeFiles/convert_imageset.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hena/caffe-ocr/tools/convert_imageset.cpp" "/home/hena/caffe-ocr/buildcmake/tools/CMakeFiles/convert_imageset.dir/convert_imageset.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CAFFE_VERSION=1.0.0-rc5"
+  "USE_CUDNN"
+  "USE_LEVELDB"
+  "USE_LMDB"
+  "USE_OPENCV"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "."
+  "/home/hena/tool/protobuf-3.1.0/build/install/include"
+  "/usr/include/hdf5/serial"
+  "/usr/local/cuda-8.0/include"
+  "/home/hena/tool/opencv-3.2.0/include"
+  "/home/hena/tool/opencv-3.2.0/include/opencv"
+  "/usr/include/atlas"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hena/caffe-ocr/buildcmake/src/caffe/CMakeFiles/caffe.dir/DependInfo.cmake"
+  "/home/hena/caffe-ocr/buildcmake/src/caffe/CMakeFiles/proto.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
